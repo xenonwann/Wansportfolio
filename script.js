5b786e149f2b37d66ab1e424b94721d34ee7d7d7
xenonwann <xenonwann@gmail.com>
2026-02-23 02:12:49 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0.3
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
